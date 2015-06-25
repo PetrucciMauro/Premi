a41c1b51228a9e0fa4f9ab1ef4c2c9f8998a0a2f
Petrucci Mauro <petruc91@gmail.com>
2015-06-25 19:08:53 +0200

    dddd

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config){
    }
 
         // optionally, configure the reporter
-    coverageReporter = {
+    coverageReporter : {
       type : 'html',
       dir : 'coverage/'
     }

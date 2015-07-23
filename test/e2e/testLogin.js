3c2ed3cdde12edbc3a86b365bd7818d0211e3b45
PetrucciMauro <petruc91@gmail.com>
2015-07-23 18:41:16 +0200

    test che non funziona

--- test/e2e/testLogin.js
+++ test/e2e/testLogin.js
@@ -7,10 +7,6 @@ describe('Premi Login page ', function() {
    
    beforeEach(function(){
     browser.get('http://localhost:8081/#/login');
-    username.clear();
-    pass.clear();
-    usernameError.clear();
-    passwordError.clear();
     
 });
 
@@ -35,18 +31,17 @@ describe('Premi Login page ', function() {
     //element(by.model('user.username')).clear();
     //element(by.model('user.password')).clear();
     
-    //var loginButton = element( by.css('[ng-click="login()"]') );
+    loginButton = element( by.css('[ng-click="login()"]') );
     //var usernameError = element(by.binding('usernameError'));
     //var passwordError = element(by.binding('passwordError'));
     
     //expect(element(by.binding('usernameError')).getText()).not.toBe('');
     //expect(element(by.binding('passwordError')).getText()).not.toBe('');
-    
-    //expect(usernameError.getAttribute('value')).toEqual("")
-    //expect(passwordError.getAttribute('value')).toEqual("")
-     
-    
- });
- 
+    usernameError.getAttribute('value').then(function(val){ expect(val).toEqual('Inserire username');
+        });
+    passwordError.getAttribute('value').then(function(val){ expect(val).toEqual('Inserire password');
+        });
+
+});
 
 });

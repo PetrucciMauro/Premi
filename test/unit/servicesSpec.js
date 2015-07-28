bab8fc6c61c0b7d90a64215eb42087a776d53283
PetrucciMauro <petruc91@gmail.com>
2015-07-28 18:12:51 +0200

    aggiunti test servizi

---
+++ test/unit/servicesSpec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+describe('service',function() {
+
+
+  //load modules
+  
+  beforeEach(function(){
+     module('premiApp');
+  });
+  
+  // Test service availability
+  it('check the existence of Main factory', inject(function(Main) {
+  expect(Main).toBeDefined();
+  }));
+    it('check the existence of toPages factory', inject(function(toPages) {
+  expect(toPages).toBeDefined();
+  }));
+    it('check the existence of Utils factory', inject(function(Utils) {
+  expect(Utils).toBeDefined();
+  }));
+    it('check the existence of Upload factory', inject(function(Upload) {
+  expect(Upload).toBeDefined();
+  }));
+    it('check the existence of SharedData factory', inject(function(SharedData) {
+  expect(SharedData).toBeDefined();
+
+  }));
+
+});
+

60d85ea4e6ab5e72c9edc97b98b72b2486f03f41
pinotto <tollotpietro@outlook.com>
2015-08-13 17:14:59 +0200

    comunicazione  x giovanni

--- serverRelation/MongoRelation.js
+++ serverRelation/MongoRelation.js
@@ -85,6 +85,7 @@ var MongoRelation = function(hostname, auth_obj){
 			//return res.success;
 			callback();
 		};
+		console.log("GIOVANNI GUARDA QUA E DIMMI COSA C'E' =) :: "+element_updated);
 		req.send(JSON.stringify(objSend));
 	};
 	

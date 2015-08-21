46bdce3ea09cacd5ea5ac4ae5527a88b6bc3d58e
pinotto <tollotpietro@outlook.com>
2015-08-21 19:51:58 +0200

    header testServer&rel

---
+++ testServer&Rel/testAuthentication.js
@@ -0,0 +1,271 @@
+/*
+ * Name : Pietro Gabelli
+ * Module : UnitTest
+ * Location : testServer&Rel/testAuthentication.js
+ *
+ */
+
+var assert = require("assert");
+var MongoClient = require('mongodb').MongoClient;
+var database = "mongodb://localhost/premi";
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var host = "http://localhost:8081";
+var fs = require('fs');
+var rmdir = require('rimraf');
+
+
+describe("authentication", function(){
+			
+
+			describe("Register", function(){
+						
+						beforeEach(function(done) {
+								 MongoClient.connect(database, function(err, db) {
+															db.collection('users').remove({}, function(err, doc){
+																									if(err) return done(err);
+																									var dir= __dirname+'/../../files/';
+																									rmdir.sync(dir);
+																									fs.mkdirSync(dir);
+                                                  db.close();
+																									done();
+																									});
+															});
+								 });
+						
+						after(function(done) {
+								MongoClient.connect(database, function(err, db) {
+														  db.collection('users').remove({}, function(err, doc){
+																								  if(err) return done(err);
+																								  var dir= __dirname+'/../../files/';
+																								  rmdir.sync(dir);
+																								  fs.mkdirSync(dir);
+                                                  db.close();
+																								  done();
+																								  });
+														  });
+								});
+
+									
+						it("registra utente non presente in db", function(done){
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/register', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+							var jsonResponse = JSON.parse(req.responseText);
+							
+							assert.equal(200, req.status);
+							assert.equal(true, jsonResponse.success);
+							MongoClient.connect(database, function(err, db) {
+													  db.collection('users').findOne({"username" : "provaname"}, function(err, doc){
+																								if(err) return done(err);
+																								assert.notEqual(null, doc);
+                                                           db.close();
+																								done();
+																								});
+													  });
+							});
+						
+						
+						it("non registra se già presente", function(done){
+							for(var i=0; i<2; i++){
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/register', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+							}
+							var jsonResponse = JSON.parse(req.responseText);
+							
+							assert.equal(400, req.status);
+						   assert.equal(false, jsonResponse.success);
+							MongoClient.connect(database, function(err, db) {
+													  db.collection('users').findOne({"username" : "provaname"}, function(err, doc){
+																								if(err) return done(err);
+																								assert.notEqual(null, doc);
+                                                           db.close();
+																								done();
+																								});
+													  });
+
+							});
+						
+						});
+			
+			describe("Authenticate",function(){
+						
+						beforeEach(function(done) {
+									  MongoClient.connect(database, function(err, db) {
+																 db.collection('users').remove({}, function(err, doc){
+																										 if(err) return done(err);
+																										 var dir= __dirname+'/../../files/';
+																										 rmdir.sync(dir);
+																										 fs.mkdirSync(dir);
+                                                               db.close();
+																										 done();
+																										 });
+																 });
+									  });
+						
+						after(function(done) {
+								MongoClient.connect(database, function(err, db) {
+														  db.collection('users').remove({}, function(err, doc){
+																								  if(err) return done(err);
+																								  var dir= __dirname+'/../../files/';
+																								  rmdir.sync(dir);
+																								  fs.mkdirSync(dir);
+                                                            db.close();
+																								  done();
+																								  });
+														  });
+								});
+
+						
+						it("autentica utente presente",function(){
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/register', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+
+							var req = new XMLHttpRequest();
+							req.open('GET', host+'/account/authenticate', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+							var jsonResponse = JSON.parse(req.responseText);
+							
+							assert.equal(200,req.status);
+							assert.equal(true,jsonResponse.success);
+							assert.notEqual(null,req.getResponseHeader("Authorization"));
+							
+							});
+						
+						it("autentica utente non presente",function(){
+							var req = new XMLHttpRequest();
+							req.open('GET', host+'/account/authenticate', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+							var jsonResponse = JSON.parse(req.responseText);
+							
+							assert.equal(400,req.status);
+							assert.equal(false,jsonResponse.success);
+							assert.equal(null,req.getResponseHeader("Authorization"));
+							});
+						
+						it("autentica password errata",function(){
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/register', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+							
+							var req = new XMLHttpRequest();
+							req.open('GET', host+'/account/authenticate', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"passowrdsbagliata");
+							req.send();
+							var jsonResponse = JSON.parse(req.responseText);
+							
+							assert.equal(400,req.status);
+							assert.equal(false,jsonResponse.success);
+							assert.equal(null,req.getResponseHeader("Authorization"));
+							});
+						
+						});
+			
+			describe("ChangePassword", function(){
+						
+						beforeEach(function(done) {
+									  MongoClient.connect(database, function(err, db) {
+																 db.collection('users').remove({}, function(err, doc){
+																										 if(err) return done(err);
+																										 var dir= __dirname+'/../../files/';
+																										 rmdir.sync(dir);
+																										 fs.mkdirSync(dir);
+                                                               db.close();
+																										 done();
+																										 });
+																 });
+									  });
+						
+						after(function(done) {
+								MongoClient.connect(database, function(err, db) {
+														  db.collection('users').remove({}, function(err, doc){
+																								  if(err) return done(err);
+																								  var dir= __dirname+'/../../files/';
+																								  rmdir.sync(dir);
+																								  fs.mkdirSync(dir);
+                                                            db.close();
+																								  done();
+																								  });
+														  });
+								});
+						
+						it("utente presente",function(done){
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/register', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/changepassword', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass"+":"+"newpass");
+							req.send();
+							var jsonResponse = JSON.parse(req.responseText);
+
+							assert.equal(200,req.status);
+							assert.equal(true,jsonResponse.success);
+							MongoClient.connect(database, function(err, db) {
+													  db.collection('users').findOne({"username" : "provaname"}, function(err, doc){
+																								if(err) return done(err);
+																								assert.equal("newpass", doc.password);
+                                                           db.close();
+																								done();
+																								});
+													  });
+							});
+						
+						it("utente non presente",function(done){
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/changepassword', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass"+":"+"newpass");
+							req.send();
+							var jsonResponse = JSON.parse(req.responseText);
+							
+							assert.equal(400,req.status);
+							assert.equal(false,jsonResponse.success);
+							MongoClient.connect(database, function(err, db) {
+													  db.collection('users').findOne({"username" : "provaname"}, function(err, doc){
+																								if(err) return done(err);
+																								assert.equal(null, doc);
+                                                           db.close();
+																								done();
+																								});
+													  });
+							});
+						
+						it("password errata",function(done){
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/register', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"provapass");
+							req.send();
+							
+							var req = new XMLHttpRequest();
+							req.open('POST', host+'/account/changepassword', false);
+							req.setRequestHeader("Authorization", "provaname"+":"+"passwordsbagliata"+":"+"newpass");
+							req.send();
+							var jsonResponse = JSON.parse(req.responseText);
+							
+							assert.equal(400,req.status);
+							assert.equal(false,jsonResponse.success);
+							MongoClient.connect(database, function(err, db) {
+													  db.collection('users').findOne({"username" : "provaname"}, function(err, doc){
+																								if(err) return done(err);
+																								assert.equal("provapass", doc.password);
+                                                           db.close();
+																								done();
+																								});
+													  });
+							});
+						
+						});
+			
+			});
+
+
+
---
+++ testServer&Rel/testServerRelation.js
@@ -0,0 +1,272 @@
+/*
+ * Name : Giovanni Venturelli
+ * Module : UnitTest
+ * Location : testServer&Rel/testServerRelation.js
+ *
+ */
+
+var assert = require("assert");
+var MongoClient = require('mongodb').MongoClient;
+var database = "mongodb://localhost/premi";
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var host = "http://localhost:8081";
+var fs = require('fs');
+var path = require('path');
+var rmdir = require('rimraf');
+
+var Registration = require("../Registration").Registration;
+var Authentication = require("../Authentication").Authentication;
+var MongoRelation = require("../MongoRelation").MongoRelation;
+var FileServerRelation = require("../FileServerRelation").FileServerRelation;
+var Loader = require("../Loader").Loader;
+
+var config = function(done){
+	MongoClient.connect(database, function(err, db) {
+							  db.collection('users').remove({}, function(err, doc){
+																	  if(err) return done(err);
+																	  db.collection('presentationsuser').remove({}, function(err, doc){
+																																	 var dir= __dirname+'/../../files/';
+																																	 rmdir.sync(dir);
+																																	 fs.mkdirSync(dir);
+                                                                              db.close();
+																																	 done();
+																																	 });
+																	  });
+							  });
+};
+
+beforeEach(function(done) {
+			  config(done);
+			  });
+
+after(function(done) {
+		config(done);
+		});
+
+describe("Presentations", function(done){
+			
+			it("PresentationMeta", function(){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var token = auth.getToken();
+				
+				var req = new XMLHttpRequest();
+				req.open('POST', host+'/private/api/presentations/new/prova_presentazione', false);
+				req.setRequestHeader("Authorization", token);
+				req.send();
+				
+				var mongoRel = MongoRelation(host, auth);
+				var metas = mongoRel.getPresentationsMeta();
+				
+				assert.equal("prova_presentazione", metas[0].titolo);
+				
+				});
+			
+			it("NewPresentation", function(){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				var metas = mongoRel.getPresentationsMeta();
+				assert.equal("prova_presentazione", metas[0].titolo);
+				});
+			
+			it("NewCopyPresentation", function(){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+
+				mongoRel.newCopyPresentation("prova_presentazione","nuova_presentazione");
+				
+				var metas = mongoRel.getPresentationsMeta();
+				assert.equal(2, metas.length);
+
+				});
+			
+			it("GetPresentation", function(){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				var pres = mongoRel.getPresentation("prova_presentazione");
+				assert.equal("prova_presentazione", pres.meta.titolo);
+
+				});
+			
+			it("DeletePresentation", function(){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				mongoRel.deletePresentation("prova_presentazione");
+				
+				var metas = mongoRel.getPresentationsMeta();
+				assert.equal(0, metas.length);
+				});
+			
+			it("renamePresentation", function(){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				mongoRel.renamePresentation("prova_presentazione","renamed_presentazione");
+				
+				var metas = mongoRel.getPresentationsMeta();
+				assert.equal("renamed_presentazione", metas[0].titolo);
+				});
+			
+			it("NewElement", function(done){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				var obj = {
+				"id": 3,
+				"xIndex": 10,
+				"yIndex": 20,
+				"rotation": 2,
+				"height": 15,
+				"width": 13,
+				"type": "image"
+				};
+				
+				mongoRel.newElement("prova_presentazione", obj, function(){
+										  var pres = mongoRel.getPresentation("prova_presentazione");
+										  assert.equal(13, pres.proper.images[0].width);
+										  done();
+										  });
+				});
+			
+			it("updateElement", function(done){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				var obj = {
+				"id": 3,
+				"xIndex": 10,
+				"yIndex": 20,
+				"rotation": 2,
+				"height": 15,
+				"width": 13,
+				"type": "image"
+				};
+				
+				mongoRel.newElement("prova_presentazione", obj, function(){
+										  var pres = mongoRel.getPresentation("prova_presentazione");
+										  assert.equal(13, pres.proper.images[0].width);
+										  obj.width = 100;
+										  mongoRel.updateElement("prova_presentazione", obj, function(){
+																		 var pres = mongoRel.getPresentation("prova_presentazione");
+																		 assert.equal(100, pres.proper.images[0].width);
+																		 done();
+																		 });
+										  });
+				});
+			
+			it("deleteElement", function(done){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				var obj = {
+				"id": 3,
+				"xIndex": 10,
+				"yIndex": 20,
+				"rotation": 2,
+				"height": 15,
+				"width": 13,
+				"type": "image"
+				};
+				
+				mongoRel.newElement("prova_presentazione", obj, function(){
+										  mongoRel.deleteElement("prova_presentazione", obj.type, obj.id, function(){
+																		 var pres = mongoRel.getPresentation("prova_presentazione");
+																		 assert.equal(0, pres.proper.images.length);
+																		 done();
+																		 });
+										  });
+				});
+			
+			it("updatePaths", function(done){
+				var reg = Registration(host);
+				reg.register("user","pass");
+				
+				var auth = Authentication(host);
+				auth.authenticate("user","pass");
+				
+				var mongoRel = MongoRelation(host, auth);
+				mongoRel.newPresentation("prova_presentazione");
+				
+				var obj_paths = {
+				"main": [2,12],
+				"choices": [{
+								"pathId": 0,
+								"choicePath": [80],
+								"type": "choice" //new!
+								}, {
+								"pathId": 1,
+								"choicePath": [11],
+								"type": "choice" //new!
+								}]
+				};
+			
+				mongoRel.updatePaths("prova_presentazione", obj_paths, function(){
+											
+											var pres = mongoRel.getPresentation("prova_presentazione");
+											assert.equal(12, pres.proper.paths.main[1]);
+											done();
+											});
+				
+				});
+			});
+
+
+
+
+
+
